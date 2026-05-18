cad7b5c08cedc7ee56f119da6f52722b21d8fb1c
Kutman Sulaimanov <kutman.sulaimanov@alatoo.edu.kg>
2026-05-19 04:22:52 +0600

    feat: initialize spring boot project with core dependencies

---
+++ src/main/java/com/replai/backend/ReplaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.replai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplaiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplaiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/replai/backend/ReplaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplaiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
